23123508ed7071fd12475072bb91373d377bba7e
zaverstaiu <https://github.com/zaverstaiu/botanika.git>
2021-08-30 19:09:41 +0300

    fixed menu

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(function () {
 
   $('.mobile-btn').on('click', function () {
     $('.mobile-menu__list').toggleClass('mobile-menu__list--open');
-    $('.mobile-btn__menu').toggleClass('mobile-btn__menu--open');
-    $('.mobile-btn__close').toggleClass('mobile-btn__close--open');
+    $('.mobile-btn').toggleClass('mobile-btn--open');
+    $('body').toggleClass('lock');
   });
 });
