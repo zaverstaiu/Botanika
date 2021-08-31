fd768aecb394f300508a006a1b2d8de102a082d4
zaverstaiu <https://github.com/zaverstaiu/botanika.git>
2021-08-31 10:54:06 +0300

    add tooltip

--- js/main.js
+++ js/main.js
@@ -5,4 +5,33 @@ $(function () {
     $('.mobile-btn').toggleClass('mobile-btn--open');
     $('body').toggleClass('lock');
   });
+
+
+  $(function () { // всплывающая подсказка
+    $("button").each(function (b) {//элемент (кнопка)
+      if (this.title) {
+        var c = this.title;
+        var x = 25;//расположение по горизонтали(left)
+        var y = 30;//расположение по вертикали (top)
+        $(this).mouseover(function (d) {
+          this.title = "";
+          $("body").append('<div id="tooltip">' + c + "</div>");
+          $("#tooltip").css({
+            left: (d.pageX + x) + "px",
+            top: (d.pageY + y) + "px",
+            opacity: "1"//полупрозрачность
+          }).show(500);//скорость появления подсказки
+        }).mouseout(function () {
+          this.title = c;
+          $("#tooltip").remove();
+        }).mousemove(function (d) {
+          $("#tooltip").css({
+            left: (d.pageX + x) + "px",
+            top: (d.pageY + y) + "px"
+          });
+        });
+      }
+    });
+  });
+
 });
